4d96bd9890a3316894d923dfe1be96715b93a0c0
TymoteuszSzarlej <tszarlej@proton.me>
2025-08-10 01:50:45 +0200

    zaczęto prace nad kontaktem

--- WWW/contact/urls.py
+++ WWW/contact/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contact_view, name='contact'),
-    path('start/', views.start_conversation, name='start_conversation'),
-    path('conversation/<int:pk>/', views.conversation_detail, name='conversation_detail'),
-    path('conversation/<int:pk>/send-message/', views.send_message, name='send_message'),
+    path('', views.contact, name='contact'),
+    path('start/', views.contact, name='start_conversation'),
+    path('conversation/<int:pk>/', views.contact, name='conversation_detail'),
+    path('conversation/<int:pk>/send-message/', views.contact, name='send_message'),
 ]
